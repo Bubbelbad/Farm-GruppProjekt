12b31297e4f0d2e80d0122a8ddb9a11480fdcfb9
BranislavDokic <143610751+BranislavDokic@users.noreply.github.com>
2023-10-16 15:22:15 +0200

    first commit

--- 2023.10.16/Crop.cs
+++ 2023.10.16/Crop.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace _2023._10._16
 {
-    internal class Crop
+    internal class Crop : Entity
     {
+
+        public string cropTyp { get; set; }
+        private int quantity { get; set; }
+        public Crop(string cropTyp, int quantit)
+        {
+            this.cropTyp = cropTyp;
+            this.quantity = quantit;
+        }
+        public override string GetDescription()
+        {
+            return base.GetDescription();
+        }
+        public int addCrop()
+        {
+            return quantity;
+        }
+        public int takeCrop()
+        {
+            return quantity;
+        }
     }
 }
